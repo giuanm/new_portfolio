805df4fbec5d69e44ac3fdbe207e639b155cce55
giuanm <giuanm@live.com>
2024-01-27 20:16:12 +0100

    alguns ajustes gerais no layout

--- src/components/Postagens_Personal/Forro.js
+++ src/components/Postagens_Personal/Forro.js
@@ -7,7 +7,7 @@ const Forro = () => {
   return (
     <>
       {!loading && (<div className='postes'>
-        <h1>Forro</h1>
+        <h1>Forró</h1>
         <p className="descricao">{data.forro}</p>
         <iframe className="videoHobbies" src="https://www.youtube.com/embed/mYa03J9MfqA?si=Kt_oR5Tchq7vsU0G" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>)}
