13d26f8c28e9dd43c31aa99fc5cfb8af5869b212
giuanm <giuanm@live.com>
2024-02-03 16:12:12 +0100

    alguns ajustes gerais no layout

--- src/components/Postagens_Personal/Malta.js
+++ src/components/Postagens_Personal/Malta.js
@@ -10,7 +10,7 @@ const Malta = () => {
         <a href="https://www.google.com/search?sca_esv=602020555&sxsrf=ACQVn0_HQ0wz0H_N-17bXtIRM69BL6m0Jg:1706376371971&q=malta&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjBiYSsi_6DAxXFQfEDHS7KBbMQ0pQJegQIDRAB&biw=1536&bih=730&dpr=1.25" target="_blank" rel="noreferrer">
           <h1>Malta</h1>
         </a>
-        <p className="descricao">{data.lisbonPT}</p>
+        <p className="descricao">{data.malta}</p>
         <h3>St. Julian's</h3>
         <iframe className="videoHobbies" src="https://www.youtube.com/embed/R7gOuTsblSk?si=6h2MayDIC079UhKV" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         <h3>Comino</h3>
